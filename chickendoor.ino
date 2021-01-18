0c6e20ef2c250532c65389935a89cda43c853038
Christoph Steinbeck <er@doktor-steinbeck.de>
2021-01-18 21:32:52 +0100

    Working version of light sensor model
    The system now waits for a given time if the light is above or below a threshold and then opens or closes the door.

--- chickendoor.ino
+++ chickendoor.ino
@@ -1,17 +1,14 @@
 #include <Wire.h>
 #include <BH1750.h>
 #include <BME280I2C.h>
-#include <NTPClient.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include <ezTime.h>
+#include <Ticker.h>
 #include "secrets.h"
 
 const char ssid[] = WIFI_SSID;
 const char password[] = WIFI_PASSWD;
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
-
+Timezone myTZ;
 BH1750 lightMeter;
 BME280I2C bme;
 int motorPinForward = D3; 
@@ -20,6 +17,23 @@ float lux;
 float temperature;
 float pressure;
 float humidity;
+/*
+ * If the light stays below minlux for minluxdelay seconds, the door closes. 
+ * If the light is above maxlux for maxluxdelay seconds, the door opens.
+ */
+float minLux = 300; 
+float maxLux = 500;
+int minLuxDelay = 15;
+int maxLuxDelay = 15;
+int minLuxSeconds = 0;
+int maxLuxSeconds = 0;
+
+boolean doorOpen = true;
+boolean doorClosed= false; 
+void checkMinLuxDuration();
+void checkMaxLuxDuration();
+Ticker minLuxTicker;
+Ticker maxLuxTicker;
 
 void setup() {
   // sets the pins as outputs:
@@ -32,22 +46,18 @@ void setup() {
     delay ( 500 );
     Serial.print ( "." );
   }
-
-  timeClient.begin();
-  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
-  // On esp8266 devices you can select SCL and SDA pins using Wire.begin(D1, D2);
+  waitForSync();
+  myTZ.setLocation(F("Europe/Berlin"));
   Wire.begin();
   lightMeter.begin();
   bme.begin();
-  // testing
-
-  //openDoor();
+  reportClimateData();
 }
 
 void loop() {
 
   readLux();
-  bme.read(pressure, temperature, humidity);
+  checkLux();
   reportData();
   delay(1000);
 }
@@ -77,12 +87,79 @@ void readLux()
   lux = lightMeter.readLightLevel();  
 }
 
+void checkLux()
+{
+  if (lux < minLux && doorOpen) minLuxTicker.attach(1, checkMinLuxDuration);
+  if (lux > maxLux && doorClosed) maxLuxTicker.attach(1, checkMaxLuxDuration);
+}
+
+
+void checkMinLuxDuration()
+{
+  Serial.print("Inside checkMinLuxDuration for ");
+  Serial.print(minLuxSeconds );
+  Serial.print(" seconds.");
+  if (lux > minLux)
+  {
+    Serial.println("Lux above threshold. Stopping door countdown.");
+    minLuxSeconds = 0;
+    minLuxTicker.detach();
+    return;
+  }
+  minLuxSeconds++;
+  if (minLuxSeconds > minLuxDelay)
+  {
+    minLuxTicker.detach();
+    
+    Serial.print("Lux has been below threshold for ");
+    Serial.print(minLuxSeconds);
+    Serial.println(" seconds. Closing door. ");
+    minLuxSeconds = 0;
+    doorClosed = true; 
+    doorOpen = false;
+  }
+}
+
+
+void checkMaxLuxDuration()
+{
+  Serial.print("Inside checkMaxLuxDuration for ");
+  Serial.print(maxLuxSeconds );
+  Serial.print(" seconds.");
+  if (lux < maxLux)
+  {
+    Serial.println("Lux below threshold. Stopping door countdown.");
+    maxLuxSeconds = 0;
+    maxLuxTicker.detach();
+    return;
+  }
+  maxLuxSeconds++;
+  if (maxLuxSeconds > maxLuxDelay)
+  {
+    maxLuxTicker.detach();
+    
+    Serial.print("Lux has been above threshold for ");
+    Serial.print(maxLuxSeconds);
+    Serial.println(" seconds. Opening door. ");
+    maxLuxSeconds = 0;
+    doorClosed = false; 
+    doorOpen = true;
+  }
+}
+
 void reportData()
 {
+  Serial.print(F("Germany:         "));
+  Serial.println(myTZ.dateTime());
+  
   Serial.print("Light: ");
   Serial.print(lux);
   Serial.println(" lx");
+}
 
+void reportClimateData()
+{
+  bme.read(pressure, temperature, humidity); 
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println("°C");
