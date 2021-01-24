36bd516dbb8a50efef28b3df2b580d2770762558
Christoph Steinbeck <er@doktor-steinbeck.de>
2021-01-24 12:25:38 +0100

    Update chickendoor.ino
    Saving state of the project before removing interupt based button handling.

--- chickendoor.ino
+++ chickendoor.ino
@@ -13,7 +13,16 @@ BH1750 lightMeter;
 BME280I2C bme;
 int motorPinForward = 18; 
 int motorPinBackward = 19; 
-int analogPin = 32;
+int motorRunningSeconds = 0;
+int motorRunDuration = 60;
+boolean motorRunning = false;
+Ticker checkMotorRunningTicker;
+int motorNumberPin;
+int buttonUpPin = 26;
+int buttonDownPin = 27;
+
+
+int analogPin = 33;
 float lux;
 float temperature;
 float pressure;
@@ -28,8 +37,15 @@ int minLuxDelay = 15;
 int maxLuxDelay = 15;
 int minLuxSeconds = 0;
 int maxLuxSeconds = 0;
-int motorRunningSeconds = 0;
-int motorRunDuration = 60;
+
+double average = 0;
+double scaleFactor = 185.0; // for 20A module = 100.0
+ // for 30A module = 66.0
+double voltage = 0.0;
+double current = 0.0;
+
+double zeroCurrent = 0.0;
+int runs = 0; 
 
 boolean doorOpen = false;
 boolean doorClosed= true; 
@@ -38,13 +54,40 @@ void checkMaxLuxDuration();
 void checkMotorRunning(int motorNumber);
 Ticker minLuxTicker;
 Ticker maxLuxTicker;
-Ticker checkMotorRunningTicker;
-int motorNumberPin;
+
+
+struct Button {
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
+};
+
+Button button_auf = {buttonUpPin, 0, false};
+Button button_zu = {buttonDownPin, 0, false};
+
+void IRAM_ATTR isr1() {
+  if (button_auf.pressed == false)
+  {
+    button_auf.numberKeyPresses += 1;
+    button_auf.pressed = true;
+  }
+  
+}
+
+void IRAM_ATTR isr2() {
+  if (button_zu.pressed == false) 
+  {
+    button_zu.numberKeyPresses += 1;
+    button_zu.pressed = true;
+  }
+  
+}
+
 
 void setup() {
   Wire.begin(16,17);
   delay(500);
-  // sets the pins as outputs:
+  // sets the two motor pins (open, close) as outputs:
   pinMode(motorPinForward, OUTPUT);
   pinMode(motorPinBackward, OUTPUT);
   Serial.begin(115200);
@@ -55,8 +98,13 @@ void setup() {
     Serial.print ( "." );
   }
   waitForSync();
+  Serial.print("Wifi connected with IP ");
+  Serial.println(WiFi.localIP());
   myTZ.setLocation(F("Europe/Berlin"));
-  
+  pinMode(button_auf.PIN, INPUT_PULLDOWN);
+  attachInterrupt(button_auf.PIN, isr1, FALLING);
+  pinMode(button_zu.PIN, INPUT_PULLDOWN);
+  attachInterrupt(button_zu.PIN, isr2, FALLING);
   lightMeter.begin();
   bme.begin();
   
@@ -66,32 +114,64 @@ void loop() {
 
   readLux();
   checkLux();
-  reportData();
+  
+  //reportData();
   //reportClimateData();
-  Serial.println(analogRead(analogPin));
+
+  for(int i = 0; i < 100; i++) {
+   average += analogRead(analogPin);
+   delay(1);
+   }
+   average /= 100;
+//   Serial.print("Average ADC current read: ");
+//   Serial.println(average);
+   voltage = ((average-775) / 4095.0) * 3300.0; // in mV
+   // for calibration to determine number 2494 at 0A current
+   // Serial.println(voltage);
+   current = voltage / scaleFactor; // in A
+    //prints current in A
+//   Serial.print("Current: ");
+//   Serial.println(current);
+
+    Serial.println(analogRead(analogPin));
+  
+//    runs++;
+//   zeroCurrent = zeroCurrent + analogRead(analogPin); 
+   
+   //Serial.print("Average Current: ");
+   //Serial.println(zeroCurrent/runs);
+  
+  handleButtonPressed();
   delay(1000);
 }
 
 
 void openDoor()
 {
-  Serial.print("Starting to open door ...");
-  moveDoor(motorPinBackward);  
-  Serial.print("Door should now be open.");
+
+  if (motorRunning == false)
+  {
+    moveDoor(motorPinBackward);  
+    Serial.println("Starting to open door ...");
+  }
+  else Serial.println("Motor already moving");
 }
 
 void closeDoor()
 {
-  Serial.print("Starting to close door ...");
-  moveDoor(motorPinForward);
-  Serial.print("Door should now be closed.");
-}
+  if (motorRunning == false)
+  {
+    moveDoor(motorPinForward);  
+    Serial.println("Starting to open door ...");
+  }
+  else Serial.println("Motor already moving");}
 
 void moveDoor(int thisMotorNumberPin)
 {
   motorNumberPin = thisMotorNumberPin;
   motorRunningSeconds = 0;
   digitalWrite(motorNumberPin, HIGH);
+  motorRunning = true;
   checkMotorRunningTicker.attach(1, checkMotorRunning);
   
 }
@@ -99,11 +179,12 @@ void moveDoor(int thisMotorNumberPin)
 void checkMotorRunning()
 {
   motorRunningSeconds ++; 
-  Serial.println(analogRead(analogPin));
+  //Serial.println(analogRead(analogPin));
   if (motorRunningSeconds >= motorRunDuration) 
   {
     Serial.println("Switching motor off");
     digitalWrite(motorNumberPin, LOW); 
+    motorRunning = false;
     checkMotorRunningTicker.detach();
     
   }
@@ -201,4 +282,19 @@ void reportClimateData()
   Serial.print(pressure/100);
   Serial.println(" mBar");
 
+}
+
+
+void handleButtonPressed()
+{
+    if (button_auf.pressed) {
+      Serial.printf("Button ''Auf'' has been pressed %u times\n", button_auf.numberKeyPresses);
+      openDoor();
+      button_auf.pressed = false;
+  }
+    if (button_zu.pressed) {
+      Serial.printf("Button ''Zu'' has been pressed %u times\n", button_zu.numberKeyPresses);
+      closeDoor();
+      button_zu.pressed = false;
+  }
 }
